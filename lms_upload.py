fa82ff5c7355d06837234899846ef6e44702ab43
timo-pi <timopiechotta@gmail.com>
2022-08-31 16:37:56 +0200

    ini-File implemented

--- lms_upload.py
+++ lms_upload.py
@@ -1,3 +1,5 @@
+import os.path
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.service import Service
@@ -7,32 +9,54 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import time
 import random
+from selenium.webdriver.common.action_chains import ActionChains
+from configparser import ConfigParser
 
-sf_url = 'https://hcm12preview.sapsf.eu/login?company=lidlstiftuT3&loginMethod=PWD#/login'
-user_name = '***'
-sf_password = '***'
-s = Service('msedgedriver.exe')
-SCORM_path = r'C:\temp2\dummy-scorm.zip'
-# SCORM_id = 'Test_310522_2'
-random_prefix = str(random.uniform(1000,9999))
+file_path = r'C:\Users\piechotta\Downloads'
+file_name = r"Testpaket_2.zip"
+#sf_url = 'https://hcm12preview.sapsf.eu/login?company=lidlstiftuT3&loginMethod=PWD#/login'
+
+config = ConfigParser()
+config.read('.\scormtester.ini')
+user_name = config.get('login', 'username')
+sf_password  = config.get('login', 'password')
+wait_time = int(config.get('settings', 'wait_time'))
+login_url = config.get('settings','login_url')
+startpage_url = config.get('settings', 'startpage_url')
+admin_center_url= config.get('settings', 'admin_center_url')
+import_content_url = config.get('settings','import_content_url')
+manage_assignments_url = config.get('settings', 'manage_assignments_url')
+
+
+edge = Service('msedgedriver.exe')
+SCORM_path = os.path.join(file_path, file_name)
+# SCORM_path = r'C:\Users\piechotta\Downloads\Testpaket.zip'
+random_prefix = str(random.uniform(0, 1))[2:6]
+scorm_id = random_prefix + "_" + file_name[:-4]
+print(scorm_id)
+driver = webdriver.Edge(service=edge)
+
 
 def start_upload(media_path):
-    driver = webdriver.Edge(service=s)
-    driver.get(sf_url)
-    print(driver.title)
+
+    driver.maximize_window()
+    driver.get(login_url)
     # driver.quit()
 
     # Login
+    next_action('__input1-inner', 'wait')
     name_field = driver.find_element(By.ID, '__input1-inner')
     password_field = driver.find_element(By.ID, '__input2-inner')
     name_field.send_keys(user_name)
     password_field.send_keys(sf_password)
+    next_action('__button2-inner', 'wait')
     driver.find_element(By.ID, '__button2-inner').click()
 
     # open Admin-Page
-    driver.get('https://lidlstiftu-stage.plateau.com/learning/admin/main.do?fromSF=y')
-    driver.get('https://lidlstiftu-stage.plateau.com/learning/admin/tools/import_content/contentAssistantStart.do?adminShellMenuId=importContent')
-
+    driver.get(admin_center_url)
+    time.sleep(wait_time)
+    driver.get(import_content_url)
+    time.sleep(wait_time)
     # Upload SCORM-File
     driver.find_element(By.ID, 'submitbutton').click()
     browse_btn = driver.find_element(By.ID, 'pickFiles')
@@ -48,79 +72,104 @@ def start_upload(media_path):
     ''')
     driver.execute_script('document.getElementById("contentDeploymentLocationID").value = "CONTENTTEST";')
     driver.execute_script('document.getElementById("cpdomain").value = "TESTING";')
-    scorm_id = random_prefix + "_Automatisierung"
+    # scorm_id = random_prefix + "_Automatisierung"
     driver.execute_script('document.getElementById("contentPackageID").value = "' + scorm_id + '";')
+
+    # **************************************************************
+    # CREATE ITEM
+    # **************************************************************
+
+    driver.find_element(By.ID, 'addNewItem').click()
+
+    driver.find_element(By.ID, 'componentTypeID').send_keys("ELEARNING")
+    driver.find_element(By.ID, 'autoGenCompID').click()
+    driver.find_element(By.ID, 'componentID').send_keys(scorm_id)
+    driver.find_element(By.ID, 'componentIDPrefix').send_keys(random_prefix)
+
+    driver.find_element(By.ID, 'componentTitle').send_keys(scorm_id)
+    driver.find_element(By.ID, 'requirementType').send_keys("Optional")
+    driver.find_element(By.ID, 'domain1').send_keys("Domain for Testing")
+
+    driver.find_element(By.ID, 'reviewable').click()
+    driver.find_element(By.ID, 'markCompleteUI').click()
+    driver.find_element(By.ID, 'skipContentStructurePage').click()
+    driver.find_element(By.ID, 'recordWhenLastObjectPassed').click()
+    next_action('onlineCompletionStatus', 'TEST_COMPL')
+
+    #driver.find_element(By.ID, 'componentID').send_keys(scorm_id)
     driver.find_element(By.ID, 'finishButton').click()
 
+
     # Wait until upload is finished
-    WebDriverWait(driver, 600).until(EC.presence_of_element_located((By.ID, 'editContentObjectIDIcon')))
+    next_action('editContentObjectIDIcon', 'wait')
+    next_action('editContentObjectIDIcon', 'click')
+    driver.switch_to.window(driver.window_handles[1])
+
+    time.sleep(wait_time)
+    next_action('contentModuleID', 'clear')
+    next_action('contentModuleID', scorm_id)
+    driver.find_element(By.NAME, 'applyChanges_bottom').click()
+
+    driver.switch_to.window(driver.window_handles[0])
+
     driver.find_element(By.ID, 'schedButton').click()
     driver.find_element(By.ID, 'submitbutton').click()
 
-    #driver.get('https://lidlstiftu-stage.plateau.com/learning/admin/main.do?fromSF=y#/learning/itementitymanager@/')
-    #print('going to sleep..........................................................................')
-
-    #time.sleep(5)
-    #print('Continue................................................................................')
-    # WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, 'addNewLink')))
-    #driver.find_element(By.XPATH, '//*[@id="addNewLink"]').click()
-
-    driver.get('https://lidlstiftu-stage.plateau.com/learning/admin/main.do?fromSF=y#/learning/itementitymanager@/core/create/item/entity/add-detail')
-    #driver.findElement(By.ID, 'item.classification-label)').click()
-    print('going to sleep..........................................................................')
-    time.sleep(5)
-    driver.find_element(By.ID, 'item.classification-label').click()
-    # list1 = driver.find_element(By.ID, '__list1')
-    driver.find_element(By.ID, '__item7-item.classification-1').click()
-
-    driver.find_element(By.ID, '__item7-item.classification-1').click()
-    driver.find_element(By.ID, 'item.componentTypeID_select-arrow').click()
-    driver.find_element(By.ID, '__item12').click()
-    driver.find_element(By.ID, 'item.title-inner').send_keys('Hallo...')
-
-    #'item.add.online.completionStatus-arrow','__item26-item.add.online.completionStatus-3','item.requirementTypeID_select-arrow','__item371','item.customColumns_30_select-arrow','__item403','item.customColumns_40_select-arrow','__item407','item.customColumns_50_select-arrow','__item411'
-
-    SCROLL_PAUSE_TIME = 0.5
-
-    # Get scroll height
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    print("last_height: ", last_height)
-
+    # Wait until Successfully Deployed
+    time.sleep(wait_time)
     while True:
-        # Scroll down to bottom
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # Wait to load page
-        time.sleep(SCROLL_PAUSE_TIME)
-
-        # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
+        time.sleep(3)
+        deployment_status = driver.execute_script('''return document.querySelector('[id*="jobStatus"]').innerHTML''')
+        if str(deployment_status) == 'Succeeded':
             break
-        last_height = new_height
-        print("new_height: ", new_height)
+        else:
+            print(deployment_status)
+    print("finished")
 
+    driver.get(manage_assignments_url)
 
+    # Manage User Learning - Form
+    # WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.NAME, 'studentNeedsForm')))
 
+    time.sleep(wait_time)
 
-    '''driver.find_element(By.ID, 'utilityLinksMenuId-inner').click()
-    driver.find_element(By.LINK_TEXT, 'Admin-Center').click()
-    # driver.implicitly_wait(4)
-    admin_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, '__link1')))
-    admin_link.click()
-    # WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
-    # driver.find_element(By.ID, '__link1').click()
-    driver.find_element(By.LINK_TEXT, 'Schulungsadministration').click()
+    next_action('addCpnt', 'click')
+    next_action('submitbutton', 'click')
+    next_action('studentId', 'click')
+    next_action('studentId', 'INT-PIECHOTTA')
+    next_action('submitbutton', 'click')
+    next_action('nextButton', 'click')
 
-    # Content Upload Form
-    driver.find_element(By.LINK_TEXT, 'Content').click()
-    driver.find_element(By.LINK_TEXT, 'Import Content').click()
-    driver.find_element(By.ID, 'submitbutton').click()
-    file = driver.find_element(By.ID, 'browseFilesButton')
-    file.send_keys('C:\\Users\\timop\\Downloads\\SK-Test.zip')
-    driver.find_element(By.ID, 'nextButton').click() '''
+    time.sleep(2)
+    next_action('addOneCpntType', 'ELEARNING')
+    next_action('id', scorm_id)
+    time.sleep(2)
+    next_action('submitbutton', 'click')
+    time.sleep(2)
+    next_action('nextButton', 'click')
+    time.sleep(2)
+    next_action('nextButton', 'click')
+    time.sleep(2)
+    next_action('runOnline', 'click')
+    time.sleep(2)
+    driver.get(startpage_url)
 
+    # Edit Item Settings
+    # driver.get('https://lidlstiftu-stage.plateau.com/learning/search/initSearch.do?searchType=0&selectorName=Component&stackID=search&entityManagerEnabled=Y')
+    # time.sleep(wait_time)
+    # next_action('componentTitle', scorm_id)
+    # next_action('search0', 'click')
+    # time.sleep(2)
+    # driver.find_element(By.PARTIAL_LINK_TEXT, 'ELEARNING').click()
 
-
+def next_action(id, action):
+    if action == 'click':
+        driver.find_element(By.ID, id).click()
+    elif action == 'wait':
+        WebDriverWait(driver, 1200).until(EC.presence_of_element_located((By.ID, id)))
+    elif action == 'clear':
+        driver.find_element(By.ID, id).clear()
+    else:
+        driver.find_element(By.ID, id).send_keys(action)
 
 start_upload("test")
